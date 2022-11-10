49939e949bcd3c0cb4803760f044d33b8e61f7cb
VitorPinheiro <vitor.vpa@gmail.com>
2022-11-10 02:38:14 -0300

    Tentando colocar a janela de data...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {MAT_DATE_LOCALE, MatNativeDateModule} from '@angular/material/core';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+
+
+const appDateFormat = {
+  parse: {
+    dateInput: 'D-M-YYYY',
+  },
+  display: {
+    dateInput: 'D-M-YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @NgModule({
   declarations: [
@@ -12,9 +29,17 @@ import { FormsModule }   from '@angular/forms';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'nl-NL' },    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
